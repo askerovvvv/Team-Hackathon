e210664dbe14241f2b158898e7cef5bb92ce30e4
Bekbol Askerov <askerov.bekbol@inbox.ru>
2022-04-13 20:51:52 +0600

    asads

--- abluvionn/settings.py
+++ abluvionn/settings.py
@@ -74,13 +74,17 @@ WSGI_APPLICATION = 'abluvionn.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test11',
+        'USER': 'hello',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
+
 
 
 # Password validation
@@ -123,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'account.CustomUser'
