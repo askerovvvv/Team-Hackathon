c88a4c7dfc32911b134622a65854745939e0542a
abluvionn <sabyraibika@gmail.com>
2022-04-17 11:26:55 +0600

    added products except image and rating

--- applications/product/admin.py
+++ applications/product/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from applications.product.models import *
-#
-# admin.site.register(Category)
-# admin.site.register(Product)
+
+admin.site.register(Category)
+admin.site.register(Product)
 # admin.site.register(Image)
 # admin.site.register(Rating)
 
